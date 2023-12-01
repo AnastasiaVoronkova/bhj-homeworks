a7db5b14e65ddb273563e2ed309c3b43c27a127d
Anastasia Voronkova <rodinastia92@mail.ru>
2023-12-01 15:44:33 +0400

    add second homeworks

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,13 @@
+let dropdownValue = document.querySelector('.dropdown__value');
+let dropdownList = document.querySelector('.dropdown__list');
+
+dropdownValue.addEventListener('click', () => {
+    dropdownList.classList.toggle('dropdown__list_active');
+});
+
+dropdownList.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let item = evt.target;
+    dropdownValue.textContent = item.textContent;
+    dropdownList.classList.remove('dropdown__list_active');
+});
